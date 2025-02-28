30fce73e65adaae8a8705dfdbe37544b48e4e44e
pm1100tm <tladnjsen@gmail.com>
2025-02-28 23:06:48 +0900

    API 문서 그룹화

--- src/main/java/com/post/prac/framework/config/OpenApiConfig.java
+++ src/main/java/com/post/prac/framework/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +18,26 @@ public class OpenApiConfig {
 	private static final String DESCRIPTION = "API Documentation";
 	private static final String VERSION = "1.0";
 	private static final String URL = "http://localhost:9080";
+
+	private static final String GROUP_NAME_AUTH = "00. AUTH";
+	private static final String GROUP_NAME_FO = "01. FO";
+
+	@Bean
+	public GroupedOpenApi authAPI() {
+		final String [] pathsToInclude = {"/api/v1/auth/**"};
+		return GroupedOpenApi.builder().group(GROUP_NAME_AUTH).pathsToMatch(pathsToInclude).build();
+	}
+
+	@Bean
+	public GroupedOpenApi foAPI() {
+		final String [] pathsToInclude = {"/api/v1/**"};
+		final String [] pathsToExclude = {"/api/v1/auth/**"};
+		return GroupedOpenApi.builder()
+				.group(GROUP_NAME_FO)
+				.pathsToExclude(pathsToExclude)
+				.pathsToMatch(pathsToInclude)
+				.build();
+	}
 
 	@Bean
 	public OpenAPI customOpenAPI() {
